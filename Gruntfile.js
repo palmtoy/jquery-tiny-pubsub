0845c5df4370643be326dbfb3d3949398d400116
Zhigang Li(Will) <zgli@kabaminc.com>
2015-11-03 17:54:04 +0800

    * simplify the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,23 +5,15 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('tiny-pubsub.jquery.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    pkgname: 'tiny-pubsub',
     // Task configuration.
     clean: {
       src: ['dist']
     },
     uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
       dist: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'dist/ba-<%= pkg.name %>.min.js'
+        src: 'src/<%= pkgname %>.js',
+        dest: 'dist/ba-<%= pkgname %>.min.js'
       },
     }
   });
