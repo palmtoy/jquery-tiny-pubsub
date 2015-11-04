39d03a5106cc70f1d5244013d7d1c77ac709bd75
Zhigang Li(Will) <zgli@kabaminc.com>
2015-11-04 11:21:07 +0800

    * add libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,29 +2,29 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkgname: 'tiny-pubsub',
-    // Task configuration.
-    clean: {
-      src: ['dist']
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkgname: 'tiny-pubsub',
+		// Task configuration.
+		clean: {
+			src: ['dist']
 		},
 		uglify: {
 			target : {
 				expand: true,
 				cwd: './',
-				src: ['src/*.js', 'views/*.js'],
+				src: ['src/*.js', 'views/*.js', 'libs/*.js'],
 				dest: './'
 			}
 		}
-  });
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task.
-  grunt.registerTask('default', ['clean', 'uglify']);
+	// Default task.
+	grunt.registerTask('default', ['clean', 'uglify']);
 
 };
